4472092944f97d2d22542ddf8f6ac9a7aaf3f198
xiakang0703 <113756127+xiakang0703@users.noreply.github.com>
2022-09-17 15:15:52 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,27 +1,27 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "wxc93158bacb37c180",
+    appId: "wxc8581494b563c8c0",
     // 公众号appSecret
-    appSecret: "8166ccc94171205acc61d79daf86d08b",
+    appSecret: "d2e7f1ef411be9283284faf4a01d2d85",
     // 模板消息id
-    templateId: "xvG5n3a5G8GETg4jKalnonM_jzTo2kOvJdXMgRdikwU",
+    templateId: "RloMF_VkE8a6ukzCWjyzW2FIlA7PolvVjwGv0EgXcaA",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: ["oKMUE5wRy-yGT4pYhh3J2o-8F7rw","oKMUE5_d2TL5BYpWZiXM1dPLP6LA"],
+    user: ["okRlz6Ngmn9ap9kyyl8SADzQI0d8"],
      
     // 信息配置
     ewai: "   ",
     // 所在省份
-    province: "广西",
+    province: "湖北",
     // 所在城市
-    city: "桂林",
+    city: "天门",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "曾曾", "year": "1998", "date": "06-02", "type": 'new'},
-      {"name": "哥哥", "year": "1996", "date": "01-07", "type": 'new'},
+      {"name": "小熊", "year": "2001", "date": "03-11", "type": 'new'},
+      {"name": "小夏", "year": "2003", "date": "07-03", "type": 'new'},
     ],
     // 在一起的日子，格式同上
-    loveDate: "2016-11-05",
+    loveDate: "2021-10-12",
     // 结婚纪念日
     
     }
